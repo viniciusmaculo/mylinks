5456c419650d37590f77a71c2e7b9230a90e102f
Vinícius Maçulo <vinimaculo@gmail.com>
2024-02-13 22:13:57 -0300

    ajusta rotas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Profile } from "../pages/profile";
 import { Admin } from "../pages/admin";
@@ -9,23 +9,43 @@ import { Error } from "../pages/error";
 
 import { Private } from "./private";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/admin" element={<Private><Admin/></Private>} />
-        <Route path="/registro" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin/social" element={<Private><Networks/></Private>} />
-        <Route path="/404" element={<Error />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter( [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/:username',
+                element: <Profile/>
+            },
+            {
+                path: '/admin',
+                element: <Private><Admin/></Private>
+            },
+            {
+                path: '/registro',
+                element: <Register/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/admin/social',
+                element: <Private><Networks/></Private>
+            },
+            {
+                path: '/404',
+                element: <Error/>
+            },
+            {
+                path: '/*',
+                element: <Error/>
+            }
+        ],
+        
+)
 
-export default App;
+export {router}
 
    
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Configuração para fallback para o index.html
+    fs: {
+      // Permitir servir arquivos do diretório raiz
+      allow: ['..'],
+    },
+  },
 })
